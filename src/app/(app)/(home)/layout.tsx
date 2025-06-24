0d2c8c177281c28aee18df7509535b8b0c025ddb
Ancol-Developer <trandieu026@gmail.com>
2025-06-24 23:42:52 +0700

    add Input Search

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,43 @@
 import { Footer } from "./footer";
+import { SearchFilter } from "./search-filters";
 import Navbar from "./navbar";
+
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+import { Category } from "@/payload-types"
 
 interface Props {
   children: React.ReactNode;
 };
 
-const Layout = ({children} : Props) => {
+const Layout = async ({children} : Props) => {
+  const payload = await getPayload({
+      config: configPromise,
+    })
+  
+  const data = await payload.find({
+    collection: 'categories',
+    pagination: false,
+    depth: 1, // Polulate Subcategory
+    where: {
+      parent: {
+        exists: false
+      },
+    },
+  })
+
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategory: (doc.subcategory?.docs ?? []).map((doc) => ({
+      // Because of "depth : 1" we are confident "doc" will be a type of "Category"
+      ...(doc as Category),
+    }))
+  }));
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <SearchFilter data={data}/>
       <div className="flex-1 bg-[#F4F4F0]">
         {children}
       </div>
