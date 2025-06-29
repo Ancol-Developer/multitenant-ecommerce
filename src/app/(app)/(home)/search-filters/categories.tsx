86bc87e7ab7c8f933f09a968af16941cfa736fe0
Ancol_Developer <trandieu026@gmail.com>
2025-06-29 23:26:32 +0700

    Add serch-filter

--- src/app/(app)/(home)/search-filters/categories.tsx
+++ src/app/(app)/(home)/search-filters/categories.tsx
@@ -8,16 +8,18 @@ export const Categories = ({
   data,
 }: Props) => {
   return (
-    <div>
-      {data.map((category : Category) => (
-        <div key={category.id}>
-          <CategoryDropDown 
-            category={category}
-            isActive={false}
-            isNavigationHovered={false}
-          />
+    <div className="relative w-full">
+      <div className="flex flex-nowrap items-center">
+        {data.map((category : Category) => (
+          <div key={category.id}>
+            <CategoryDropDown 
+              category={category}
+              isActive={false}
+              isNavigationHovered={false}
+            />
         </div>
       ))}
+      </div>
     </div>
   )
 };
--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -4,7 +4,11 @@ import { useRef, useState } from "react";
 
 import { cn } from "@/lib/utils";
 import { Category } from "@/payload-types"
+
 import { Button } from "@/components/ui/button";
+
+import { useDropdownPosition } from "./use-dropdown-position";
+import { SubcategoryMenu } from "./subcategory-menu";
 
 interface Props {
   category: Category;
@@ -19,6 +23,7 @@ export const CategoryDropDown = ({
 }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const {getDropdownPosition} = useDropdownPosition(dropdownRef);
 
   const onMouseEnter = () => {
     if (category.subcategories){
@@ -27,6 +32,8 @@ export const CategoryDropDown = ({
   }
 
   const onMouseLeave = () => setIsOpen(false);
+
+  const dropdownPosition = getDropdownPosition();
 
   return(
     <div
@@ -44,15 +51,19 @@ export const CategoryDropDown = ({
         )}>
         {category.name}
         </Button>
-        {category.subcategories && category.subcategories.length > 0 && (
+        {category.subcategories && category.subcategories.docs.length > 0 && (
           <div className={cn(
-            "opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
-            isOpen && "opacity-100"
-          )}>
-
-          </div>
+              "opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
+              isOpen && "opacity-100"
+              )}
+          />
         )}
       </div>
+      <SubcategoryMenu 
+        category= {category}
+        isOpen={isOpen}
+        position = {dropdownPosition}
+      />
     </div>
   );
 }
