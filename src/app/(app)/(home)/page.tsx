24c63c714d078fa4dc658dfd19dfc68e0024d8eb
Ancol-Developer <trandieu026@gmail.com>
2025-07-07 22:49:03 +0700

    add Category router

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {   
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+
+export default function Home() {  
+  const trpc = useTRPC();
+  const categories = useQuery(trpc.categories.getMany.queryOptions());
+  
   return (
     <div className="p-4">
-      Home page
+      {JSON.stringify(categories.data, null, 2)}
     </div>
   );
 }
---
+++ src/modules/categories/server/procedures.ts
@@ -0,0 +1,32 @@
+import { getPayload } from 'payload'
+import configPromise from '@payload-config'
+
+import { Category } from '@/payload-types';
+import { baseProcedure, createTRPCRouter } from "@/trpc/init"
+
+export const  categoriesRouter = createTRPCRouter({
+  getMany: baseProcedure.query(async ({ ctx }) => {
+    const data = await ctx.db.find({
+      collection: 'categories',
+      pagination: false,
+      depth: 1, // Polulate Subcategory, subcategories.[0] will be a type of "Category"
+      where: {
+        parent: {
+          exists: false
+        },
+      },
+      sort: "name"
+    });
+
+    const formattedData = data.docs.map((doc) => ({
+        ...doc,
+        subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+          // Because of "depth : 1" we are confident "doc" will be a type of "Category"
+          ...(doc as Category),
+          subcategories: undefined,
+        }))
+      }));
+
+    return formattedData;
+  }),
+});
