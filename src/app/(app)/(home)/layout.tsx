f30a588ddde18adb57e7f857373455bde42682ac
Ancol_Developer <trandieu026@gmail.com>
2025-07-09 23:21:58 +0700

    FIx bug

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -5,6 +5,10 @@ import { Footer } from "./footer";
 import { SearchFilter } from "./search-filters";
 import Navbar from "./navbar";
 
+import configPromise from '@payload-config'
+import { getPayload } from "payload";
+import { Category } from "@/payload-types";
+
 
 interface Props {
   children: React.ReactNode;
@@ -16,6 +20,31 @@ const Layout = async ({children} : Props) => {
   void queryClient.prefetchQuery(
     trpc.categories.getMany.queryOptions(),
   );
+
+  const payload = await getPayload({
+      config: configPromise,
+    })
+  
+  const data = await payload.find({
+    collection: 'categories',
+    pagination: false,
+    depth: 1, // Polulate Subcategory, subcategories.[0] will be a type of "Category"
+    where: {
+      parent: {
+        exists: false
+      },
+    },
+    sort: "name"
+  })
+
+  const formattedData : CustomCategory[] = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      // Because of "depth : 1" we are confident "doc" will be a type of "Category"
+      ...(doc as Category),
+      subcategories: undefined,
+    }))
+  }));
 
   return (
     <div className="flex flex-col min-h-screen">
