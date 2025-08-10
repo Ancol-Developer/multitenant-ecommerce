d384fcc1253f0fbd0ee559e91711f6de4d23c21a
Ancol_Developer <trandieu026@gmail.com>
2025-08-10 14:23:40 +0700

    Add Form Sign in

---
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,13 @@
+export const SignInView = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-col-5">
+      <div className="bg-[#F4F4F0] h-screen w-full lg:col-span-3 overflow-y-auto">
+      Form columns
+      </div>
+
+      <div className="h-screen w-full lg:col-span-2 hidden lg:block">
+        Background column
+      </div>
+    </div>
+  )
+}
