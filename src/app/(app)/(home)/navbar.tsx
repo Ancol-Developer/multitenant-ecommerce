deb3d3e80e44bfcf4f727c34826cb80cab8bdc9e
Ancol_Developer <trandieu026@gmail.com>
2025-08-17 15:35:59 +0700

    Authentication State

--- src/app/(app)/(home)/navbar.tsx
+++ src/app/(app)/(home)/navbar.tsx
@@ -6,6 +6,9 @@ import { usePathname } from "next/navigation";
 import { MenuIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 import { Button } from "@/components/ui/button";
 import  { NavbarSidebar } from "./navbar-sidebar";
 
@@ -51,6 +54,10 @@ const navbarItems = [
 const Navbar = () => {
   const pathName = usePathname();
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const trpc = useTRPC();
+  const session = useQuery(trpc.auth.session.queryOptions());
+
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
       <Link href="/" className="pl-6 flex items-center">
@@ -73,25 +80,38 @@ const Navbar = () => {
         ))}
       </div>
 
-      <div className="hidden lg:flex">
-        <Button 
-          asChild
-          variant="secondary"
-          className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white text-black hover:bg-pink-400 hover:text-white transition-colors text-lg">
-          <Link prefetch href="/sign-in">
-            Login
-          </Link>
-        </Button>
-        
-        <Button
-          asChild
-          variant="secondary"
-          className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-white transition-colors text-lg">
-          <Link prefetch href="/sign-up">
-            Start selling
-          </Link>
-        </Button>
-      </div>
+      {session.data?.user ? (
+        <div className="hidden lg:flex">
+          <Button
+            asChild
+            variant="secondary"
+            className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-white transition-colors text-lg">
+            <Link href="/admin">
+              Dashboard
+            </Link>
+          </Button>
+        </div>
+      ) : (
+        <div className="hidden lg:flex">
+          <Button 
+            asChild
+            variant="secondary"
+            className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white text-black hover:bg-pink-400 hover:text-white transition-colors text-lg">
+            <Link prefetch href="/sign-in">
+              Login
+            </Link>
+          </Button>
+          
+          <Button
+            asChild
+            variant="secondary"
+            className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-white transition-colors text-lg">
+            <Link prefetch href="/sign-up">
+              Start selling
+            </Link>
+          </Button>
+        </div>
+      )}
 
       <div className="flex lg:hidden items-center justify-center">
         <Button
--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -7,7 +7,7 @@ import { toast } from "sonner";
 import { Poppins } from "next/font/google";
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
@@ -33,11 +33,13 @@ export const SignInView = () => {
   const router = useRouter();
 
   const trpc = useTRPC();
+  const queryClient = useQueryClient();
   const login = useMutation(trpc.auth.login.mutationOptions({
     onError: (error) => {
       toast.error(error.message);
     },
-    onSuccess: () => {
+    onSuccess: async () => {
+      await queryClient.invalidateQueries(trpc.auth.session.queryFilter());
       router.push("/");
     }
   }));
